7082270815c82608dd3fbc1f1a4bc9930b522fb8
mike820324 <mike820324@gmail.com>
2015-02-26 01:04:04 +0800

    update package.json and gulpfile.js remove some modules that we dont need right now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
-var react = require('gulp-react');
-var flow = require('gulp-flowtype');
 
 var srcFolder = 'src/**/*.js';
 var destFolder = 'dist';
@@ -9,7 +7,6 @@ var destFolder = 'dist';
 gulp.task('build', function() {
 	return gulp.src(srcFolder)
 	.pipe(babel())
-	.pipe(react({stripTypes:true}))
 	.pipe(gulp.dest(destFolder));
 });
 
