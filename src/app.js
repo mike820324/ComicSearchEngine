615171eaa9143b7a33e9b176f14defa4cc1b7e30
mike820324 <mike820324@gmail.com>
2015-02-26 22:41:53 +0800

    add app.js to startup the crawler and do some indexing.

---
+++ src/app.js
@@ -0,0 +1,52 @@
+import crawler from './lib/crawler';
+import Indexer from './lib/indexer';
+
+import Opencc from 'opencc';
+const opencc = new Opencc('tw2s.json');
+
+let comicIndexer = new Indexer('./newdb');
+
+let initLinks = [
+	'http://www.comicvip.com/comic/all.html',
+	'http://mh.99770.cc/comiclist/0',
+	'http://www.99comic.com/lists/',
+	'http://www.dmeden.com/comic/'
+];
+
+// init crawlers
+let crawlers = initLinks.map(initLink => {
+	return new crawler(initLink, 1000);
+});
+
+
+let index = (err, comicList) => {
+	if(err) console.log(err);
+	else {
+		let url = comicList[0].url;
+		
+		// @fixme
+		// not a good pattern
+		if(url.indexOf('comicvip') !== -1)
+			comicIndexer.add('comicvip', comicList);
+
+		else if(url.indexOf('99770') !== -1)
+			comicIndexer.add('99770', comicList);
+		
+		else if(url.indexOf('99comic') !== -1)
+			comicIndexer.add('99comic', comicList);
+
+		else if(url.indexOf('dmeden') !== -1)
+			comicIndexer.add('dmeden', comicList);
+
+		else
+			console.log('can not find the proper indexer');
+	}
+};
+
+let main = () => {
+	for(crawler of crawlers) {
+		crawler.start(index);
+	}
+};
+
+main();
