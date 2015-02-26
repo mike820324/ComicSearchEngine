f79135595d3a43fe310f3f3384bd6b611e072e51
mike820324 <mike820324@gmail.com>
2015-02-27 03:01:26 +0800

    add some error class

---
+++ src/lib/error/index.js
@@ -0,0 +1,16 @@
+class parserError extends Error {
+	constructor(message) {
+		this.name = 'parserError';
+		this.message = message;
+	}
+}
+module.exports.parserError = parserError;
+
+class crawlerError extends Error {
+	constructor(message, url) {
+		this.name = 'crawlerError';
+		this.message = message;
+		this.url = url;
+	}
+}
+module.exports.crawlerError = crawlerError;
